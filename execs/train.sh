337ada816861796ba7706d43e43f5fac4e8bd2c5
maichi98 <elaichi.mohammed.1998@gmail.com>
2025-03-26 21:49:24 +0100

    stuff(

---
+++ execs/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ensure that the script exits if a command fails
+set -e
+
+
+# Activate the conda environment
+source "$(conda info --base)"/etc/profile.d/conda.sh
+conda activate env_oof_unet
+
+# Define the project directory:
+DIR_PROJECT="/home/maichi/work/my_projects/LySAIRI/OOF_UNET"
+
+# Run train.py :
+python "$DIR_PROJECT/oof_unet/train.py" --fold 0 --num_epochs 1 --n_units 5
